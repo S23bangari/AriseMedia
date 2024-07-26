973f27afa2ace40686a8977835af2ae2af4707f2
S23bangari <somilbangari@gmail.com>
2024-07-26 14:29:07 +0530

    card text

--- src/Components/PopularWork/PopularWork.jsx
+++ src/Components/PopularWork/PopularWork.jsx
@@ -36,7 +36,7 @@ export default function PopularWork() {
             Your browser does not support the video tag.
           </video>
           <div className="name">
-            <span className='text-2xl bg-transparent absolute bottom-24 right-20'>@MagnatesMedia</span>
+            <span className='text-2xl bg-transparent absolute bottom-24 right-14'>@MagnatesMedia</span>
             <div className="sub-view-text">
               <span className="subs">1.55M Subscribers</span>
               <span className="views">6.7M Views</span>
@@ -49,7 +49,7 @@ export default function PopularWork() {
             Your browser does not support the video tag.
           </video>
           <div className="name">
-            <span className='text-2xl bg-transparent absolute bottom-24 right-24 '>@TechnoGamerz</span>
+            <span className='text-2xl bg-transparent absolute bottom-24 right-20'>@TechnoGamerz</span>
             <div className="flex justify-center gap-1 mt-1 text-white bg-transparent  relative bottom-16 right-10">
               <span className="subs">41.5M subscribers</span>
               <span className="views">5.8 Million Views</span>
@@ -63,7 +63,7 @@ export default function PopularWork() {
             Your browser does not support the video tag.
           </video>
           <div className="name">
-            <span className='text-2xl bg-transparent absolute bottom-24 right-20'>@MagnatesMedia</span>
+            <span className='text-2xl bg-transparent absolute bottom-24 right-14'>@MagnatesMedia</span>
             <div className="sub-view-text">
               <span className="subs">1.55M Subscribers</span>
               <span className="views">4.3M Views</span>
@@ -77,7 +77,7 @@ export default function PopularWork() {
             Your browser does not support the video tag.
           </video>
           <div className="name">
-            <span className='text-2xl bg-transparent absolute bottom-24 right-24'>@VisualVenture</span>
+            <span className='text-2xl bg-transparent absolute bottom-24 right-20'>@VisualVenture</span>
             <div className="sub-view-text">
               <span className="subs">605K Subscribers</span>
               <span className="views">3.3M Views</span>
@@ -91,7 +91,7 @@ export default function PopularWork() {
             Your browser does not support the video tag.
           </video>
           <div className="name">
-            <span className='text-2xl bg-transparent absolute bottom-24 right-20'>@InternetAnarchist</span>
+            <span className='text-2xl bg-transparent absolute bottom-24 right-12'>@InternetAnarchist</span>
             <div className="sub-view-text">
               <span className="subs">1.07M Subscribers</span>
               <span className="views">1.8M Views</span>
@@ -105,7 +105,7 @@ export default function PopularWork() {
             Your browser does not support the video tag.
           </video>
           <div className="name">
-            <span className='text-2xl bg-transparent absolute bottom-24 right-20'>@InternetAnarchist</span>
+            <span className='text-2xl bg-transparent absolute bottom-24 right-12'>@InternetAnarchist</span>
             <div className="sub-view-text">
               <span className="subs">1.07M Subscribers</span>
               <span className="views">1M Views</span>
